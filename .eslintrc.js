13f4cc574c2dbe8cb1ac9b585f52106231cb0e08
ssb1565b <ssongsumovie@naver.com>
2023-03-09 22:51:33 +0900

    setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
   extends: ['airbnb-base'],
   rules: {
+    'linebreak-style': 0,
     'no-console': 'off',
+    'no-plusplus': 'off',
   },
   // node: true,
+  parseOptions: {
+    ecmaVersion: 'latest',
+  },
+  env: {
+    es6: true,
+  },
+  treatUndefinedAsUnspecified: true,
 };
